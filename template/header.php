ccf8678894517a4b65b4a11229259746b12b83c2
sebastien <55012430+speleo27@users.noreply.github.com>
2021-01-28 08:44:39 +0100

    Add files via upload

--- template/header.php
+++ template/header.php
@@ -1,7 +1,7 @@
 <?php
 require "controller/connectdb.php";
 session_start(); 
-//var_dump($_SESSION['user']);
+//var_dump($_SESSION);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,17 +15,30 @@ session_start();
     <title>Groin-groin the game</title>
 </head>
 <body>
-<nav class="navbar navbar-light bg-light">
+<nav class="navbar navbar-light bg-light  d-flex align-items-end">
   <form class="container-fluid justify-content-start">
     <?php
-   
-    if($_SERVER['SCRIPT_NAME'] != "/template/createAccount.php"){
-     if(isset($_SESSION["user"])){
-    echo'
     
-    <a href="accountChange.php?connect_id='.$_SESSION['user']['connect_id'].'" class="btn btn-outline-danger ml-2 mr-auto" type="button" >'. $_SESSION["user"]["connect_pseudo"].'</a>
+    if($_SERVER['SCRIPT_NAME'] != "/farm/createAccount.php" && $_SERVER['SCRIPT_NAME'] != "/farm/accountChange.php"){
+      if(isset($_SESSION["user"])){
 
-    <button class="btn btn-outline-danger  ml-2 mr-auto" type="button" data-bs-toggle="modal" data-bs-target="#disconnectModal">Deconnexion</button>';
+      $req=$bdd->prepare('SELECT account_amount FROM account WHERE connect_id=? LIMIT 1');
+      $req->execute(array($_SESSION['user']['connect_id']));
+      $data=$req->fetch();
+
+      $req2=$bdd->prepare('SELECT farm_name FROM farm WHERE connect_id=? LIMIT 1');
+      $req2->execute(array($_SESSION['user']['connect_id']));
+      $data2=$req2->fetch();
+
+        echo'
+            <div class="float-right">
+                <a href="accountChange.php?connect_id='.$_SESSION['user']['connect_id'].'" class="btn btn-outline-danger ml-2 mr-auto" type="button" >'. $_SESSION["user"]["connect_pseudo"]. '</a>
+                <a class="btn btn-outline-success" >'.$data2['farm_name'].'</a>
+                <a class="btn btn-outline-success" >'.$data['account_amount'].' $</a>
+                <button class="btn btn-outline-danger  ml-2 mr-auto " type="button" data-bs-toggle="modal" data-bs-target="#buildingModal">Batiment</button>
+                <button class="btn btn-outline-danger  ml-2 mr-auto " type="button" data-bs-toggle="modal" data-bs-target="#disconnectModal">Deconnexion</button>
+            </div>';
+
     }else{
         echo'
     <button class="btn btn-outline-success ml-auto" type="button" data-bs-toggle="modal" data-bs-target="#connectModal">Connexion</button>';
